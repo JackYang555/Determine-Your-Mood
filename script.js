36a4e18cb3c9ecaa4ef30fed3c34f1ac07ca990b
JackYang555 <43828896+JackYang555@users.noreply.github.com>
2019-05-07 15:57:26 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,44 @@
+var happy=["https://mindbodygreen-res.cloudinary.com/image/upload/w_767,q_auto:eco,f_auto,fl_lossy/ftr/woman-abundance.jpg","https://www.silverhccenter.com/wp-content/uploads/2018/07/happy-mood.jpg","https://crazyyetwise.files.wordpress.com/2011/01/happy.jpg"];
+var sad=["https://image.shutterstock.com/image-photo/portrait-man-sad-mood-people-450w-1024988086.jpg","https://cdn.shopify.com/s/files/1/1061/1924/products/Sad_Face_Emoji_large.png?v=1480481055","https://cdn4.iconfinder.com/data/icons/emoticons-27/64/disappoint-512.png"];
+var annoyed=["https://www.thelocal.ch/userdata/images/article/890bc41b95d5a4eca1a33e96f359f38de1fc8e4578aee0e1c3a81ba7753b1cef.jpg","https://i.pinimg.com/originals/56/92/d0/5692d096777213a7f713c9841ba04a66.png","http://experience.sweetflag.club/webhook-uploads/1468611931240/1333982900_kristen-stewart-lg06.jpg"];
+var fear=["https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4Ny8yMDUvb3JpZ2luYWwvc2NpZW5jZS1vZi1mZWFyLmpwZw==","https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/face-screaming-in-fear.png","https://www.thepainproject.com/wp-content/uploads/2017/01/iStock-519492437-min-910x510.jpg"];
+
+$('button').click(function(){
+    var input = $('input').val();
+    $(".images").html("");
+    if(input === "happy"){
+        happyMood();
+    } else if(input==="sad"){
+        sadMood();
+    }else if(input==="annoyed"){
+        annoyedMood();
+    }else if(input==="fearful"){
+        fearfulMood();
+    }else{
+        alert("Please enter happy, annoyed, sad or fearful");   
+    }
+});
+
+function happyMood(){
+    displayImage(happy);
+}
+
+function sadMood(){
+    displayImage(sad);
+}
+
+function annoyedMood(){
+    displayImage(annoyed);
+}
+
+function fearfulMood(){
+    displayImage(fear);
+}
+function displayImage(image){
+    happy.forEach(function(element){
+    $(".images").append("<img src ="+ image + ">");    
+    });
+}
+
+
+
